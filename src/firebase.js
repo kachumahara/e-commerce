df7348bc49054a67eaee957dbbe08c6d0b2a0df9
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-09-10 00:29:51 -0700

    set up firebase and header compononet

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAiYqpA2Xf0n22JNTr_gXsrUPFU64kP_zs",
+  authDomain: "e-commerce-2fdd5.firebaseapp.com",
+  databaseURL: "https://e-commerce-2fdd5.firebaseio.com",
+  projectId: "e-commerce-2fdd5",
+  storageBucket: "e-commerce-2fdd5.appspot.com",
+  messagingSenderId: "529839296872",
+  appId: "1:529839296872:web:a9c66be0ca5d1db3b606dd",
+  measurementId: "G-VBT5F6SKJT"
+};
