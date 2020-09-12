ec2b85928a46f64e4d1b5fce7cb1f0b2665959ce
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-09-12 01:59:44 -0700

    added the login functionalities using firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAiYqpA2Xf0n22JNTr_gXsrUPFU64kP_zs",
   authDomain: "e-commerce-2fdd5.firebaseapp.com",
@@ -6,5 +9,13 @@ const firebaseConfig = {
   storageBucket: "e-commerce-2fdd5.appspot.com",
   messagingSenderId: "529839296872",
   appId: "1:529839296872:web:a9c66be0ca5d1db3b606dd",
-  measurementId: "G-VBT5F6SKJT"
+  measurementId: "G-VBT5F6SKJT",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
